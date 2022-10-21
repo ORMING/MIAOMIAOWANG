9ae146088a1002da8479ff46be039be9efd0fc3d
DESKTOP-KP1FL5B\MING <122104049@stu.jp-sji.org>
2022-10-21 16:37:42 +0900

    123

---
+++ TestDoWhile.java
@@ -0,0 +1,11 @@
+public class TestDoWhile{
+	public static void main(String[] args) {
+		int x=10;
+		
+		do {
+			System.out.println("x的值"+x);
+			x++;
+			System.out.println("\n");
+		}while(x<20);
+	}
+}
---
+++ TestForLoop.java
@@ -0,0 +1,9 @@
+public class TestForLoop{
+	public static void main(String[] args) {
+		
+		for(int x=10;x<20;x=++x) {
+			System.out.println("x的值"+x);
+			System.out.println("\n");
+		}
+	}
+}
