f04791ee58ace9e52b8cb8591e26123e39c5f5eb
DESKTOP-KP1FL5B\MING <122104049@stu.jp-sji.org>
2022-10-24 21:42:42 +0900

    221024

---
+++ Hallo.java
@@ -0,0 +1,14 @@
+
+//这是Java的快速入门，演示Java的开发步骤
+//对代码的相关说明
+//1.pulic class Hello 表示Hello是一个类，是一个public公有的类
+//2.hello{}表示一个类的开始和结束
+//3.public static void main(String[] args)表示一个主方法，即我们程序的入口
+//4.main(){}表示方法的开始和结束
+//5System.out.println("hello,world~");表示输出“hello，world~”到屏幕
+//;代变语句结束
+public class Hallo {
+	public static void main(String[] args) {
+		System.out.println("hello.world~");
+	}
+}
