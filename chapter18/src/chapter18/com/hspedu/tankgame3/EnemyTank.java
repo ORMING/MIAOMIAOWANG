4527f3ef529b50069e235622c8623579fd2987ec
ORMING <115915562+ORMING@users.noreply.github.com>
2023-09-26 21:58:22 +0900

    Revert "Delete chapter18 directory"
    This reverts commit a6e57ac341467541f8376c40b0824134edef291e.

---
+++ chapter18/src/chapter18/com/hspedu/tankgame3/EnemyTank.java
@@ -0,0 +1,10 @@
+package chapter18.com.hspedu.tankgame3;
+
+public class EnemyTank extends Tank{
+
+	public EnemyTank(int x, int y) {
+		super(x, y);
+		// TODO 自动生成的构造函数存根
+	}
+	
+}
---
+++ chapter18/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module chapter18 {
+	requires java.desktop;
+}
